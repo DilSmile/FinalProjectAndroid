ce5a0f60aeb66b59c865525de42e11bb0b6cbcb0
Merey <210103180@stu.sdu.edu.kz>
2024-05-08 01:37:30 +0500

    Included utils file

---
+++ app/src/main/java/com/example/finalprojectmovie/common/Const.kt
@@ -0,0 +1,13 @@
+package com.example.finalprojectmovie.common
+
+object Const {
+    object Api {
+        const val BASE_URL = "https://api.themoviedb.org/"
+        const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
+        const val IMAGE_URL = "https://image.tmdb.org/t/p/original/"
+        const val MOVIE_STARTING_PAGE_INDEX = 1
+        const val MOVIES_PAGE_SIZE = 20
+        const val TRAILER = "Trailer"
+    }
+
+}
