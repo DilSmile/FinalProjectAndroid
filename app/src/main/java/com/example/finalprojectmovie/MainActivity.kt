ff7f7325fce37a4c2c2df3bc60cf846fb7825190
Dilnaz <210103077@stu.sdu.edu.kz>
2024-05-07 18:12:34 +0500

    Added fragments,and const file

--- app/src/main/java/com/example/finalprojectmovie/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectmovie/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.finalprojectmovie
+package com.example.finalprojectmovie.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.finalprojectmovie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
