d41606b13efdabd15513dc59a6157e12900d050a
Dilnaz <210103077@stu.sdu.edu.kz>
2024-05-08 12:34:55 +0500

    Setup retrofit,added api file with model

---
+++ app/src/main/java/com/example/finalprojectmovie/network/model/MovieResponse.kt
@@ -0,0 +1,10 @@
+package com.example.finalprojectmovie.data.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val movies: List<MovieEntity>
+)
