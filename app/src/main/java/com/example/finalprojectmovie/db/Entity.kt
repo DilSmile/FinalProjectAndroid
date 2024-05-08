491ee8ef7119177019969b0da7de4280f5acdba9
Dilnaz <210103077@stu.sdu.edu.kz>
2024-05-08 14:14:10 +0500

    Setup Room Database,work with database

---
+++ app/src/main/java/com/example/finalprojectmovie/db/Entity.kt
@@ -0,0 +1,20 @@
+package com.example.finalproject.data.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "watch_list")
+data class WatchListMovieEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo("movie_id")
+    val movie_id: Int,
+    @ColumnInfo("is_favorite")
+    val is_favorite: Boolean,
+    @ColumnInfo("poster_path")
+    val posterPath: String,
+    @ColumnInfo("title")
+    val title: String,
+    @ColumnInfo("genre")
+    val genre: String
+)
