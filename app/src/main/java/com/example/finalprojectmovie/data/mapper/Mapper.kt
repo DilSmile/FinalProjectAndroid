b53d84b2e4228c0ddf0dcf73d09611a07b681e5f
Merey <210103180@stu.sdu.edu.kz>
2024-05-08 15:36:43 +0500

    Added mapper file,and in const file included genres

---
+++ app/src/main/java/com/example/finalprojectmovie/data/mapper/Mapper.kt
@@ -0,0 +1,75 @@
+package com.example.finalprojectmovie.data.mapper
+
+import com.example.finalprojectmovie.common.Const.Api.IMAGE_URL
+import com.example.finalprojectmovie.common.Const.Api.YOUTUBE_URL
+import com.example.finalprojectmovie.common.round
+import com.example.finalprojectmovie.data.db.WatchListMovieEntity
+import com.example.finalprojectmovie.data.network.model.GenreEntity
+import com.example.finalprojectmovie.data.network.model.MovieDetailsEntity
+import com.example.finalprojectmovie.data.network.model.MovieEntity
+import com.example.finalprojectmovie.data.network.model.VideoEntity
+import com.example.finalprojectmovie.domain.model.Movie
+import com.example.finalprojectmovie.domain.model.MovieDetails
+import com.example.finalprojectmovie.domain.model.Trailer
+import com.example.finalprojectmovie.domain.model.Video
+
+fun MovieEntity.mapToDomain(): Movie {
+    return Movie(
+        id = this.id,
+        title = this.title,
+        posterUrl = IMAGE_URL + this.posterPath,
+        rating = (this.rating / 2).round(1),
+    )
+}
+
+fun MovieDetailsEntity.mapToDomain(): MovieDetails {
+    return MovieDetails(
+        id = this.id,
+        title = this.title,
+        posterUrl = IMAGE_URL + this.posterPath,
+        rating = (this.rating / 2).round(1),
+        description = this.description,
+        releaseDate = this.releaseDate,
+        voteCount = this.voteCount,
+        genres = this.genres.map(GenreEntity::mapToDomain)
+    )
+}
+
+fun GenreEntity.mapToDomain(): String {
+    return this.name
+}
+
+fun VideoEntity.mapToDomain(): Video {
+    return Video(
+        type = this.type,
+        name = this.name,
+        url = YOUTUBE_URL + this.key
+    )
+}
+
+fun VideoEntity.mapToTrailer(): Trailer {
+    return Trailer(
+        name = this.name,
+        url = YOUTUBE_URL + this.key
+    )
+}
+
+fun WatchListMovieEntity.mapToDomain(): Movie {
+    return Movie(
+        id = this.movie_id,
+        title = this.title,
+        posterUrl = IMAGE_URL + this.posterPath,
+        rating = null,
+        genre = this.genre
+    )
+}
+
+fun Movie.mapToWatchListEntity(): WatchListMovieEntity {
+    return WatchListMovieEntity(
+        movie_id = this.id,
+        is_favorite = false,
+        posterPath = this.posterUrl.removePrefix(YOUTUBE_URL),
+        title = this.title,
+        genre = this.genre
+    )
+}
