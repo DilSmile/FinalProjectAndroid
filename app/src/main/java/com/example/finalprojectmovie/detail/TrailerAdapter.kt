2403fe6e358c41c4fa87ec68ef132c0c99879e21
Dilnaz <210103077@stu.sdu.edu.kz>
2024-05-09 21:26:10 +0500

    Added movie detail fragment files,and updates

---
+++ app/src/main/java/com/example/finalprojectmovie/detail/TrailerAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.finalprojectmovie.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalprojectmovie.databinding.ItemTrailerBinding
+import com.example.finalprojectmovie.domain.model.Trailer
+
+class TrailerAdapter : ListAdapter<Trailer, TrailerAdapter.ViewHolder>(TrailersDiffUtils) {
+
+    var onClick: ((url: String) -> Unit)? = null
+
+    class ViewHolder(
+        private val binding: ItemTrailerBinding,
+        private val onClick: ((url: String) -> Unit)?
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(listItem: Trailer) {
+            binding.title.text = listItem.name
+            binding.root.setOnClickListener {
+                onClick?.invoke(listItem.url)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemTrailerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ),
+            onClick
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    object TrailersDiffUtils : DiffUtil.ItemCallback<Trailer>() {
+        override fun areItemsTheSame(oldItem: Trailer, newItem: Trailer): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Trailer, newItem: Trailer): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+}
