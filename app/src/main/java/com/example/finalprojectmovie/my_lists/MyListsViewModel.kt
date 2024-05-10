a4f46695844c6eaafabd36f997aa635a6a463242
Merey <210103180@stu.sdu.edu.kz>
2024-05-10 13:45:00 +0500

    Added movie my list fragment with viewmodel,adapter

---
+++ app/src/main/java/com/example/finalprojectmovie/my_lists/MyListsViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.finalprojectmovie.presentation.screen.my_lists
+
+import com.example.finalprojectmovie.domain.model.Movie
+import com.example.finalprojectmovie.domain.repository.MovieRepository
+import com.example.finalprojectmovie.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class MyListsViewModel @Inject constructor(
+    private val repository: MovieRepository
+) : BaseViewModel() {
+
+    val watchList: Flow<List<Movie>> = repository.getWatchListMovies()
+
+}
